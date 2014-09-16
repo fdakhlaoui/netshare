422edd843056f597b5a4a074036f05268c92e722
fdakhlaoui <fdakhlaoui@gmail.com>
2014-09-16 14:51:13 +0100

    Update Pom.xml

--- netshare/src/fdakhlaoui/netshare/Main.java
+++ netshare/src/fdakhlaoui/netshare/Main.java
@@ -3,7 +3,7 @@ package fdakhlaoui.netshare;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
+		System.out.println("Hello World from develop branch !");
 	}
 
 }
